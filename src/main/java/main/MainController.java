7a386a7f8d1bd9bf92a65ccf8d847b89488c6f4b
Doomslayer1001 <danushsoosai28@gmail.com>
2026-04-29 20:43:20 -0400

    The Tab Customer can display the data in the Database

--- src/main/java/main/MainController.java
+++ src/main/java/main/MainController.java
@@ -1,19 +1,50 @@
 package main;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import Customer.*;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import util.DBConnectionUtility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.List;
 
 public class MainController {
 
     @FXML
-    private Label messageLabel;
+    private TableView<Customer> Table;
+    @FXML
+    private TableColumn<Customer, Integer> coID;
+    @FXML
+    private TableColumn<Customer, String> coFN;
+    @FXML
+    private TableColumn<Customer, String> coLN;
+    @FXML
+    private TableColumn<Customer, String> coPN;
+    @FXML
+    private TableColumn<Customer, String> coE;
+
+    private CustomerDAO dao;
 
     @FXML
-    private Button clickButton;
+    public void initialize() {
+        coID.setCellValueFactory(data -> new javafx.beans.property.SimpleIntegerProperty(data.getValue().getCustomerID()).asObject());
+        coFN.setCellValueFactory(data -> new javafx.beans.property.SimpleStringProperty(data.getValue().getFirstName()));
+        coLN.setCellValueFactory(data -> new javafx.beans.property.SimpleStringProperty(data.getValue().getLastName()));
+        coPN.setCellValueFactory(data -> new javafx.beans.property.SimpleStringProperty(data.getValue().getPhoneNumber()));
+        coE.setCellValueFactory(data -> new javafx.beans.property.SimpleStringProperty(data.getValue().getEmail()));
+        dao = new CustomerDAOImpl();
+
+    }
 
     @FXML
-    private void handleButtonClick() {
-        messageLabel.setText("Button clicked!");
+    private void Display() {
+        List<Customer> customers = dao.getAllCustomers();
+        Table.setItems(FXCollections.observableArrayList(customers));
     }
 }
