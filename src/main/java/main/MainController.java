265ea8da5cff310737ffee0868b35603215e863d
Doomslayer1001 <danushsoosai28@gmail.com>
2026-05-02 18:29:54 -0400

    .

--- src/main/java/main/MainController.java
+++ src/main/java/main/MainController.java
@@ -39,7 +39,7 @@ public class MainController {
             }
         });
 
-        loadCustomers(); // 🔥 THIS MUST BE HERE
+        loadCustomers();
     }
     private void loadCustomers() {
         List<Customer> customers = dao.getAllCustomers();
